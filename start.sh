ae3405ee335ec5b4cdcedf0dd3719ea55c8fa784
DGFR <100404424+danangfir@users.noreply.github.com>
2025-12-10 18:55:28 +0700

    Add passport:install to startup script

--- start.sh
+++ start.sh
@@ -3,6 +3,8 @@
 echo "Running migrations..."
 php artisan migrate:fresh --force --seed
 
+echo "Installing Passport keys..."
+php artisan passport:install --force
+
 echo "Starting server..."
 php artisan serve --host=0.0.0.0 --port=$PORT
-
