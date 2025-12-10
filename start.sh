94ce288de5430cb8e6f8a0c4b8c4817ce8d529fa
DGFR <100404424+danangfir@users.noreply.github.com>
2025-12-10 18:57:40 +0700

    Fix: Add passport:keys and passport:client

--- start.sh
+++ start.sh
@@ -3,8 +3,11 @@
 echo "Running migrations..."
 php artisan migrate:fresh --force --seed
 
-echo "Installing Passport keys..."
-php artisan passport:install --force
+echo "Generating Passport keys..."
+php artisan passport:keys --force
+
+echo "Installing Passport clients..."
+php artisan passport:client --personal --no-interaction
 
 echo "Starting server..."
 php artisan serve --host=0.0.0.0 --port=$PORT
